989d4a1963b31a16b2ad5ad5060ce497b27f1427
gokemon <michael.of.gokey@gmail.com>
2017-04-02 20:48:00 -0400

    done with 3-10, working on 3-11

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 console.log('Starting app.js');
 const fs = require('fs'); //file system
-
+const os = require('os');
 const notes = require('./notes.js');
 
+var result = notes.addNotes();
+console.log(result);
 
-//const os = require('os');
-//var user = os.userInfo();
-//console.log(user);
-//fs.appendFileSync('greetings.txt', 'Hello ${user.username}!');
+console.log('Result:',notes.addNumbers(9,-2));
+// var user = os.userInfo();
+// console.log(user);
+// fs.appendFileSync('greetings.txt', 'Hello ${user.username}! You are ${note.age}');
+
+
--- notes.js
+++ notes.js
@@ -1,5 +1,21 @@
 console.log('Starting notes.js');
 const fs = require('fs');
-console.log(module);
+// console.log('Printing out the module details:', module);
 
+/*makes age and its value available to use elsewhere */
 module.exports.age = 49;
+
+/* example of how arrow function looks */
+// module.exports.addNotes = fucntion(){
+//     // this is an exmple of an anonymous function
+//     // the one below uses an arrow function
+// }
+
+module.exports.addNotes = ()=>{
+    console.log('add note');
+    return 'New note';
+};
+
+module.exports.addNumbers = (a,b)=>{
+    return a + b;
+};
